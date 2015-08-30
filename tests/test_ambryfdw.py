4d502e6aa331cff9c70c909df4ad21d4f63614a7
nmb10 <nmb10@dhcppc2.home>
2015-08-30 12:25:25 +0300

    Date and datetime columns processing added.

--- tests/test_ambryfdw.py
+++ tests/test_ambryfdw.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime, date
 import os
 from unittest import TestCase
 
@@ -21,9 +22,9 @@ class TestAmbryFdw(TestCase):
 
     def test_generates_integers(self):
         options = {
-            'filename': os.path.join(TEST_FILES_DIR, 'header_2_columns_100_rows_all_integers.msg')}
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_int_header_and_100_rows_gzipped.msg')}
         quals = []
-        columns = ['a', 'b']
+        columns = ['rowid', 'col1']
         wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 100)
@@ -31,13 +32,35 @@ class TestAmbryFdw(TestCase):
 
     def test_generates_strings(self):
         options = {
-            'filename': os.path.join(TEST_FILES_DIR, 'header_2_columns_100_rows_all_strings.msg')}
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_str_header_and_100_rows_gzipped.msg')}
         quals = []
-        columns = ['a', 'b']
+        columns = ['rowid', 'col1']
         wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 100)
-        self.assertEquals(ret[0], ['0', '0'])
+        self.assertEquals(ret[0], [0, '0'])
+
+    def test_generates_dates(self):
+        options = {
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_date_header_and_100_rows_gzipped.msg')}
+        quals = []
+        columns = ['rowid', 'col1']
+        wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
+        ret = list(wrapper.execute(quals, columns))
+        self.assertEquals(len(ret), 100)
+        self.assertEquals(ret[0], [0, date(2015, 8, 30)])
+
+    def test_generates_datetimes(self):
+        options = {
+            'filename': os.path.join(
+                TEST_FILES_DIR,
+                'rowid_int_col1_datetime_header_and_100_rows_gzipped.msg')}
+        quals = []
+        columns = ['rowid', 'col1']
+        wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
+        ret = list(wrapper.execute(quals, columns))
+        self.assertEquals(len(ret), 100)
+        self.assertEquals(ret[0], [0, datetime(2015, 8, 30, 12, 9, 10, 681995)])
 
     @fudge.patch('ambryfdw.log_to_postgres')
     def test_logs_unknown_qual(self, fake_log):
@@ -46,31 +69,31 @@ class TestAmbryFdw(TestCase):
             .with_args(arg.contains('Unknown operator'), WARNING, hint=arg.contains('Implement'))
 
         options = {
-            'filename': os.path.join(TEST_FILES_DIR, 'header_2_columns_100_rows_all_integers.msg')}
-        columns = ['a', 'b']
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_str_header_and_100_rows_gzipped.msg')}
+        columns = ['rowid', 'col1']
         wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
-        quals = [Qual('a', '?', 3)]
+        quals = [Qual('col1', '?', '3')]
         list(wrapper.execute(quals, columns))
 
     # quals tests
     def test_equals_operator(self):
         """ tests = operator. """
         options = {
-            'filename': os.path.join(TEST_FILES_DIR, 'header_2_columns_100_rows_all_integers.msg')}
-        columns = ['a', 'b']
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_str_header_and_100_rows_gzipped.msg')}
+        columns = ['rowid', 'col1']
         wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
-        quals = [Qual('a', '=', 3)]
+        quals = [Qual('col1', '=', '3')]
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 1)
-        self.assertEquals(ret[0], [3, 3])
+        self.assertEquals(ret[0], [3, '3'])
 
     def test_less_than_operator(self):
         """ tests < operator. """
         options = {
-            'filename': os.path.join(TEST_FILES_DIR, 'header_2_columns_100_rows_all_integers.msg')}
-        columns = ['a', 'b']
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_int_header_and_100_rows_gzipped.msg')}
+        columns = ['rowid', 'col1']
         wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
-        quals = [Qual('a', '<', 3)]
+        quals = [Qual('col1', '<', 3)]
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 3)
         self.assertEquals(ret[0], [0, 0])
@@ -79,10 +102,10 @@ class TestAmbryFdw(TestCase):
 
     def test_greater_than_operator(self):
         options = {
-            'filename': os.path.join(TEST_FILES_DIR, 'header_2_columns_100_rows_all_integers.msg')}
-        columns = ['a', 'b']
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_int_header_and_100_rows_gzipped.msg')}
+        columns = ['rowid', 'col1']
         wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
-        quals = [Qual('a', '>', 10)]
+        quals = [Qual('col1', '>', 10)]
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 89)
         self.assertEquals(ret[0], [11, 11])
@@ -92,10 +115,10 @@ class TestAmbryFdw(TestCase):
     def test_less_than_or_equal_operator(self):
         """ tests <= operator. """
         options = {
-            'filename': os.path.join(TEST_FILES_DIR, 'header_2_columns_100_rows_all_integers.msg')}
-        columns = ['a', 'b']
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_int_header_and_100_rows_gzipped.msg')}
+        columns = ['rowid', 'col1']
         wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
-        quals = [Qual('a', '<=', 1)]
+        quals = [Qual('col1', '<=', 1)]
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 2)
         self.assertEquals(ret[0], [0, 0])
@@ -104,10 +127,10 @@ class TestAmbryFdw(TestCase):
     def test_greater_than_or_equal_operator(self):
         """ tests >= operator. """
         options = {
-            'filename': os.path.join(TEST_FILES_DIR, 'header_2_columns_100_rows_all_integers.msg')}
-        columns = ['a', 'b']
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_int_header_and_100_rows_gzipped.msg')}
+        columns = ['rowid', 'col1']
         wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
-        quals = [Qual('a', '>=', 98)]
+        quals = [Qual('col1', '>=', 98)]
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 2)
         self.assertEquals(ret[0], [98, 98])
@@ -116,10 +139,10 @@ class TestAmbryFdw(TestCase):
     def test_not_equal_operator(self):
         """ tests <> operator. """
         options = {
-            'filename': os.path.join(TEST_FILES_DIR, 'header_2_columns_100_rows_all_integers.msg')}
-        columns = ['a', 'b']
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_int_header_and_100_rows_gzipped.msg')}
+        columns = ['rowid', 'col1']
         wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
-        quals = [Qual('a', '<>', 0)]
+        quals = [Qual('col1', '<>', 0)]
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 99)
         self.assertEquals(ret[0], [1, 1])
@@ -127,34 +150,32 @@ class TestAmbryFdw(TestCase):
     def test_like_operator(self):
         """ tests ~~ operator. """
         options = {
-            'filename': os.path.join(TEST_FILES_DIR, 'header_2_columns_100_rows_all_strings.msg')}
-        columns = ['a', 'b']
+            'filename': os.path.join(TEST_FILES_DIR, 'rowid_int_col1_str_header_and_100_rows_gzipped.msg')}
+        columns = ['rowid', 'col1']
         wrapper = PartitionMsgpackForeignDataWrapper(options, columns)
 
-        quals = [Qual('a', '~~', '1')]
+        quals = [Qual('col1', '~~', '1')]
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 1)
-        self.assertEquals(ret[0], ['1', '1'])
+        self.assertEquals(ret[0], [1, '1'])
 
-        quals = [Qual('a', '~~', '%1')]
+        quals = [Qual('col1', '~~', '%1')]
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 10)
-        self.assertEquals(ret[0], ['1', '1'])
-        self.assertEquals(ret[1], ['11', '11'])
-        self.assertEquals(ret[-1], ['91', '91'])
+        self.assertEquals(ret[0], [1, '1'])
+        self.assertEquals(ret[1], [11, '11'])
+        self.assertEquals(ret[-1], [91, '91'])
 
-        quals = [Qual('a', '~~', '1%')]
+        quals = [Qual('col1', '~~', '1%')]
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 11)
-        self.assertEquals(ret[0], ['1', '1'])
-        self.assertEquals(ret[1], ['10', '10'])
-        self.assertEquals(ret[-1], ['19', '19'])
+        self.assertEquals(ret[0], [1, '1'])
+        self.assertEquals(ret[1], [10, '10'])
+        self.assertEquals(ret[-1], [19, '19'])
 
-        quals = [Qual('a', '~~', '_')]
+        quals = [Qual('col1', '~~', '_')]
         ret = list(wrapper.execute(quals, columns))
         self.assertEquals(len(ret), 10)
-        self.assertEquals(ret[0], ['0', '0'])
-        self.assertEquals(ret[1], ['1', '1'])
-        self.assertEquals(ret[-1], ['9', '9'])
-
-    # FIXME: Test date and time objects.
+        self.assertEquals(ret[0], [0, '0'])
+        self.assertEquals(ret[1], [1, '1'])
+        self.assertEquals(ret[-1], [9, '9'])
